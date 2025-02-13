edfab3f4141d07cf4a92c43ced80d8b43987181e
woeislolo <falloutgirl7@mail.ru>
2025-02-13 12:54:41 +0500

    Add automatic user profile creation for social auth

--- celebs/settings.py
+++ celebs/settings.py
@@ -132,12 +132,25 @@ AUTH_PASSWORD_VALIDATORS = [
 # auth
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    # 'men.authentification.EmailAuthBackend',
+    # 'men.authentication.EmailAuthBackend',
     'social_core.backends.google.GoogleOAuth2',
     ]
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config['GOOGLE_OAUTH2_KEY']
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config['SECRET']
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config['GOOGLE_OAUTH2_SECRET']
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'men.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    ]
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
