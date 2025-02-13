5101590061ff45b667e6d2bf7ead0ff535a420df
woeislolo <falloutgirl7@mail.ru>
2025-02-13 12:19:24 +0500

    Add validation on existing email by registration anf profile updating

--- men/forms.py
+++ men/forms.py
@@ -56,6 +56,12 @@ class RegisterUserForm(UserCreationForm):
         model = get_user_model()
         fields = ('username', 'email', 'password1', 'password2')
 
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email уже используется.')
+        return data
+
 
 class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label='Логин', 
@@ -87,7 +93,18 @@ class ProfileUpdateForm(forms.ModelForm):
     last_name = forms.CharField(max_length=32, 
                                 label='Фамилия',
                                 widget=forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.CharField(max_length=32, 
+                            label='Email',
+                            widget=forms.TextInput(attrs={'class': 'form-input'}))
 
     class Meta:
         model = Profile
-        fields = ['photo', 'date_of_birth',]
+        fields = ['first_name', 'last_name', 'date_of_birth', 'email', 'photo']
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id)\
+            .filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError('Email уже используется.')
+        return data
